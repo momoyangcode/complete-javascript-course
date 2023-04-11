bb6bc10ed0e46e4581502443fc52b4ee5438f6b8
Momo Yang <momo.yang666@gmail.com>
2023-04-11 13:13:12 +0200

    write code on my own

--- 06-Modal/review/script.js
+++ 06-Modal/review/script.js
@@ -1,1 +1,39 @@
 'use strict';
+
+//Select Elements
+const showModalBtn = document.querySelectorAll('.show-modal');
+const modal = document.querySelector('.modal');
+const closeModalBtn = document.querySelector('.close-modal');
+const overLay = document.querySelector('.overlay');
+
+//Function-- open hidden modal
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+//Function-- close modal
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+//Open Modal-- click show modal button
+for (i = 0; i < showModalBtn.length; i++) {
+  showModalBtn[i].addEventListener('click', openModal);
+}
+
+//Close Modal--Three Ways
+
+//click ✖️ button to close modal
+closeModalBtn.addEventListener('click', closeModal);
+
+//click overlay part to close modal
+overLay.addEventListener('click', closeModal);
+
+//press esc to close modal
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
